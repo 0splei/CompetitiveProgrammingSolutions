7ac2718e3e515e8401ce4fad572e2458e107aeb1
0splei <josepleites23@gmail.com>
2026-09-09 23:31:15 -0600

    Weird Algorithm

---
+++ CSES/Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<ll, ll> LL;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vl;
+typedef vector<LL> vll;
+
+#define pb push_back
+#define F first
+#define S second
+
+void SOLVE(){
+
+    ll n;
+
+    cin >> n;
+    cout << n << " ";
+
+    while (n != 1) cout << (n & 1 ? n = (n * 3) + 1 : n /= 2) << " ";
+
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  //cin >> t;
+  while(t--){
+    SOLVE();
+  }
+  return 0;
+}
