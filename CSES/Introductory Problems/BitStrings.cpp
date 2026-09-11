b3a6f40cf6d53274626771de6acadedcf5b20dbc
0splei <josepleites23@gmail.com>
2026-09-10 23:53:10 -0600

    Bit Strings

---
+++ CSES/Introductory Problems/BitStrings.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<ll, ll> LL;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vl;
+typedef vector<LL> vll;
+
+#define pb push_back
+#define F first
+#define S second
+
+void SOLVE(){
+
+    ll n, mod = 1e9 + 7, ans = 1;
+    cin >> n;
+    
+    for (ll i = 1; i <= n; i++) {
+
+        ans *= 2;
+        ans %= mod;
+
+    }
+
+    cout << ans << "\n";
+
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  //cin >> t;
+  while(t--){
+    SOLVE();
+  }
+  return 0;
+}
