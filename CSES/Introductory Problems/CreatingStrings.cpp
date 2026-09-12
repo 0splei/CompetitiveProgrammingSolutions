de25205c97fa959d0adac37051e8cfd9d66eb1ea
0splei <josepleites23@gmail.com>
2026-09-11 23:11:33 -0600

    Creating Strings

---
+++ CSES/Introductory Problems/CreatingStrings.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<ll, ll> LL;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vl;
+typedef vector<LL> vll;
+
+#define pb push_back
+#define F first
+#define S second
+
+void SOLVE(){
+
+    string p;
+    set <string> option;
+
+    cin >> p;
+
+    sort(p.begin(), p.end());
+    option.insert(p);
+
+    while (next_permutation(p.begin(), p.end())) option.insert(p);
+
+    cout << option.size() << "\n";
+
+    for (auto i : option) cout << i << "\n";
+
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  //cin >> t;
+  while(t--){
+    SOLVE();
+  }
+  return 0;
+}
